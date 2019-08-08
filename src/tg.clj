05682d8aa04e2acdb1511a8d47c69a6bec753f69
Mikhail Gusarov <dottedmag@dottedmag.net>
2019-08-09 00:56:02 +0200

    DI for Telegram sender

--- src/tg.clj
+++ src/tg.clj
@@ -1,22 +1,23 @@
 (ns tg
-  (:require [org.httpkit.client :as http]
+  (:require [fx]
             [cheshire.core :as cheshire]))
 
 (def base "https://api.telegram.org/bot")
 
 (defn send-message
-  ([token chat-id message-id text]
+  ([http token chat-id message-id text]
    (let [url (str base token (if message-id "/editMessageText" "/sendMessage"))
          body {:chat_id chat-id :text text :message_id message-id :parse_mode "Markdown"}]
-     @(http/post url {:headers {"Content-Type" "application/json"}
-                      :body (cheshire/generate-string body)
-                      :form-params  body}))))
+     (http nil {:url url
+                 :headers {"Content-Type" "application/json"}
+                 :body (cheshire/generate-string body)
+                 :form-params  body}))))
 
 (comment
   (def chat-id (read-line))
   (def token (read-line))
   (defn s [message-id text]
-    (send-message token chat-id message-id text))
+    (send-message fx/http token chat-id message-id text))
 
   (def resp (s nil "test from REPL"))
   (def body (cheshire/parse-string (:body resp) keyword))
@@ -30,4 +31,5 @@
                                 :commit "ea7f20b8672acaba97ff1e73d19856372b8109aa"
                                 :steps [{:name "beat off the meat" :state :succeeded}
                                         {:name "bread the meat" :state :succeeded}
-                                        {:name "fry the meat" :state :succeeded}]})))
+                                        {:name "fry the meat" :state :succeeded}]}))
+  )
